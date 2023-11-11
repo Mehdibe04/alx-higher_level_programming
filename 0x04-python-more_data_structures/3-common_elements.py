ce57f978cb807c00bfecf48b54bdc519af7e3c73
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-11-10 17:15:06 -0800

    updates

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    commonalit= set(set_1) - (set(set_1) - set(set_2))
-    return (commonalit)
+    c = set(set_1) - (set(set_1) - set(set_2))
+    return (c)
