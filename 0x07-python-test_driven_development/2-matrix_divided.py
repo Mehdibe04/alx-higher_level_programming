7c4241515410f0db8cd0d7d6a13229ca394c8eca
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-12-01 18:44:08 -0800

    Updates

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defines a matrix division function."""
-
+"""
+Defines a matrix div func
+"""
 
 def matrix_divided(matrix, div):
     """Divide all elmts of a matrx
@@ -13,20 +14,33 @@ def matrix_divided(matrix, div):
     Returns:
         A new matrx representg the res of the div
     """
-    if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(row, list) for row in matrix) or
-            not all((isinstance(elmts, int) or isinstance(elmts, float))
-                    for elmts in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
 
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-
-    if not isinstance(div, int) and not isinstance(div, float):
+    if not type(div) in (int, float):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    return ([list(map(lambda y: round(y / div, 2), row)) for row in matrix])
+    m_type = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError(m_type)
+
+    l_e = 0
+    m_size = "Each row of the matrix must have the same size"
+
+    for ele in matrix:
+        if not ele or not isinstance(ele, list):
+            raise TypeError(m_type)
+
+        if l_e != 0 and len(ele) != l_e:
+            raise TypeError(m_size)
+
+        for num in ele:
+            if not type(num) in (int, float):
+                raise TypeError(m_type)
+
+        l_e = len(ele)
+
+    g = list(map(lambda i: list(map(lambda j: round(j / div, 2), i)), matrix))
+    return (g)
